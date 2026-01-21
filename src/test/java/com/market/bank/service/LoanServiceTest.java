fd49e2ac9bf9ed0b9f7cb3334801cf4b3be861d9
Santiago Torres <tiiago616@gmail.com>
2026-01-21 10:46:40 -0500

    add Unit test and dependencies for it

--- src/test/java/com/market/bank/service/LoanServiceTest.java
+++ src/test/java/com/market/bank/service/LoanServiceTest.java
@@ -6,6 +6,14 @@ import com.market.bank.util.LoanStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class LoanServiceTest {
 
